cb35114207f659789d6f38698191e1f2efd27f54
Scott de Jong <s.dejong@rocketlab.co.nz>
2019-09-08 08:52:02 +1200

    proper types. generic file handling

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,38 +1,97 @@
 export namespace OFX {
-  export interface asdas {
-    CCSTMTTRNRS: {
-      AVAILBAL: any;
-      BANKTRANLIST: {
-        DTEND: string;
-        DTSTART: string;
-        STMTTRN: {
-          /** Date */
-          DTPOSTED: string;
-          FITID: string;
-          MEMO: string;
-          /** Recipient */
-          NAME: string;
-          /** Amount */
-          TRNAMT: string;
-          TRNTYPE: string;
-        }[]
-      };
-      CCACCTFROM: {
-        ACCTID: string;
-      };
-      CURDEF: string;
-      LEDGERBAL: {
-        BALAMT: string;
-        DTASOF: string;
-      }
-    }
+  export interface File {
+    OFX: Ofx;
   }
-  export interface OfxData {
-    header: any;
-    OFX: {
-      CREDITCARDMSGSRSV1?: asdas;
-      BANKMSGSRSV1?: asdas;
-      SIGNONMSGSRSV1: any;
-    };
+
+  export interface Ofx {
+      SIGNONMSGSRSV1:     Signonmsgsrsv1;
+      CREDITCARDMSGSRSV1: Creditcardmsgsrsv1;
+      BANKMSGSRSV1:   Bankmsgsrsv1;
+  }
+
+  export interface Bankmsgsrsv1 {
+      STMTTRNRS: Stmttrnrs;
+  }
+
+  export interface Creditcardmsgsrsv1 {
+      CCSTMTTRNRS: Ccstmttrnrs;
+  }
+
+  export interface Stmttrnrs {
+      TRNUID: string;
+      STATUS: Status;
+      STMTRS: Stmtrs;
+  }
+
+  export interface Ccstmttrnrs {
+      TRNUID:   string;
+      STATUS:   Status;
+      CCSTMTRS: Ccstmtrs;
+  }
+
+  export interface Stmtrs {
+      CURDEF:       string;
+      BANKACCTFROM: Bankacctfrom;
+      BANKTRANLIST: Banktranlist;
+      LEDGERBAL:    BAL;
+      AVAILBAL:     BAL;
+  }
+
+  export interface Bankacctfrom {
+      BANKID:   string;
+      ACCTID:   string;
+      ACCTTYPE: string;
+  }
+
+  export interface Ccstmtrs {
+      CURDEF:       string;
+      CCACCTFROM:   Ccacctfrom;
+      BANKTRANLIST: Banktranlist;
+      LEDGERBAL:    BAL;
+      AVAILBAL:     BAL;
+  }
+
+  export interface BAL {
+      BALAMT: string;
+      DTASOF: string;
+  }
+
+  export interface Banktranlist {
+      DTSTART: string;
+      DTEND:   string;
+      STMTTRN: Stmttrn[];
+  }
+
+  export interface Stmttrn {
+      TRNTYPE:  Trntype;
+      DTPOSTED: string;
+      TRNAMT:   string;
+      FITID:    string;
+      NAME:     string;
+      MEMO:     string;
+  }
+
+  export enum Trntype {
+      Debit = "DEBIT",
+  }
+
+  export interface Ccacctfrom {
+      ACCTID: string;
+  }
+
+  export interface Status {
+      CODE:     string;
+      SEVERITY: string;
+      MESSAGE:  string;
+  }
+
+  export interface Signonmsgsrsv1 {
+      SONRS: Sonrs;
+  }
+
+  export interface Sonrs {
+      STATUS:   Status;
+      DTSERVER: string;
+      LANGUAGE: string;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,26 @@
 import * as fs from 'fs-extra';
+import * as path from 'path';
 import * as xml from 'xml2js';
 import { OFX } from './interfaces';
 
+const INDIR = 'input';
+
 (async function main() {
-  const ofxString = await fs.readFile('input/tranhist.ofx').then(f => f.toString());
+  const files = await fs.readdir(INDIR);
+  const ofxFiles = files.filter(f => f.endsWith('.ofx'));
+  
+  for (const filePath of ofxFiles) {
+    const ofxString = await fs.readFile(path.join(INDIR, filePath)).then(f => f.toString());
 
-  const ofxData: OFX.OfxData = await xml.parseStringPromise(ofxString, {
-    mergeAttrs: true,
-    explicitArray: false,
-  });
+    const ofxData: OFX.File = await xml.parseStringPromise(ofxString, {
+      explicitArray: false,
+    });
 
-  const statementResponse = ofxData.OFX.CREDITCARDMSGSRSV1.STMTTRNRS.STMTRS;
-  const accountId = statementResponse.BANKACCTFROM.ACCTID;
-  const currencyCode = statementResponse.CURDEF;
-  const transactionStatement = statementResponse.BANKTRANLIST.STMTTRN;
-  // do something...  
+    const statementResponse = ofxData.OFX.CREDITCARDMSGSRSV1.STMTTRNRS.STMTRS;
+    const accountId = statementResponse.BANKACCTFROM.ACCTID;
+    const currencyCode = statementResponse.CURDEF;
+    const transactionStatement = statementResponse.BANKTRANLIST.STMTTRN;
+    // do something...  
+  }
 
 })()
