46456de066731c9041a672af0ae40860ab6b94c0
Scott de Jong <s.dejong@rocketlab.co.nz>
2019-09-08 08:20:32 +1200

    get it running

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-const foo = "";
+import { parse as parseOFX } from 'ofx-js';
+import * as fs from 'fs-extra';
+
+(async function main() {
+  const ofxString = await fs.readFile('input/tranhist.ofx').then(f => f.toString());
+
+  const ofxData = await parseOFX(ofxString)
+
+  const statementResponse = ofxData.OFX.CREDITCARDMSGSRSV1.STMTTRNRS.STMTRS;
+  const accountId = statementResponse.BANKACCTFROM.ACCTID;
+  const currencyCode = statementResponse.CURDEF;
+  const transactionStatement = statementResponse.BANKTRANLIST.STMTTRN;
+  // do something...  
+
+})()
